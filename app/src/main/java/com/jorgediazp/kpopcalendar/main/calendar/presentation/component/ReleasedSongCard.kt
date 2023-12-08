79dc35af29ecd742383d94fdf996d805b264bea0
Jorge Díaz Pérez <jorge.diaz@alten.es>
2023-12-08 13:47:41 +0100

    create ThumbnailView

--- app/src/main/java/com/jorgediazp/kpopcalendar/main/calendar/presentation/component/ReleasedSongCard.kt
+++ app/src/main/java/com/jorgediazp/kpopcalendar/main/calendar/presentation/component/ReleasedSongCard.kt
@@ -1,21 +1,11 @@
 package com.jorgediazp.kpopcalendar.main.calendar.presentation.component
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.PlayArrow
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,12 +13,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
 import com.jorgediazp.kpopcalendar.common.theme.KpopCalendarTheme
 
 @Composable
@@ -64,35 +51,11 @@ fun SongCard(
             if (loadVideo) {
                 YoutubeVideoPlayer(youtubeURL = youtubeURL, backgroundColor = cardColor)
             } else {
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    contentAlignment = Alignment.Center
-                ) {
-                    AsyncImage(
-                        model = thumbnailUrl,
-                        contentDescription = null,
-                        contentScale = ContentScale.FillWidth,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .aspectRatio(1.8f)
-                    )
-                    Box(
-                        Modifier
-                            .matchParentSize()
-                            .background(cardColor.copy(alpha = 0.2f))
-                    )
-                    Button(
-                        onClick = { loadVideo = true },
-                        colors = ButtonDefaults.buttonColors(containerColor = buttonColor)
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.PlayArrow,
-                            contentDescription = null,
-                            modifier = Modifier.size(32.dp)
-                        )
-                    }
-                }
+                ThumbnailView(
+                    thumbnailUrl = thumbnailUrl,
+                    cardColor = cardColor,
+                    buttonColor = buttonColor,
+                    onPlayClick = { loadVideo = true })
             }
         }
     }
---
+++ app/src/main/java/com/jorgediazp/kpopcalendar/main/calendar/presentation/component/ThumbnailView.kt
@@ -0,0 +1,57 @@
+package com.jorgediazp.kpopcalendar.main.calendar.presentation.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+
+@Composable
+fun ThumbnailView(
+    thumbnailUrl: String,
+    cardColor: Color,
+    buttonColor: Color,
+    onPlayClick: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ) {
+        AsyncImage(
+            model = thumbnailUrl,
+            contentDescription = null,
+            contentScale = ContentScale.FillWidth,
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(1.8f)
+        )
+        Box(
+            Modifier
+                .matchParentSize()
+                .background(cardColor.copy(alpha = 0.2f))
+        )
+        Button(
+            onClick = { onPlayClick() },
+            colors = ButtonDefaults.buttonColors(containerColor = buttonColor)
+        ) {
+            Icon(
+                imageVector = Icons.Default.PlayArrow,
+                contentDescription = null,
+                modifier = Modifier.size(32.dp)
+            )
+        }
+    }
+}
