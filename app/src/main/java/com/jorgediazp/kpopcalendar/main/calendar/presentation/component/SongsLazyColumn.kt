2721b5652c63458dd966714bbf2f021b2c9589b2
Jorge Díaz Pérez <jorge.diaz@alten.es>
2023-12-12 17:45:17 +0100

    add space at the end of lazycolumn

--- app/src/main/java/com/jorgediazp/kpopcalendar/main/calendar/presentation/component/SongsLazyColumn.kt
+++ app/src/main/java/com/jorgediazp/kpopcalendar/main/calendar/presentation/component/SongsLazyColumn.kt
@@ -2,12 +2,16 @@ package com.jorgediazp.kpopcalendar.main.calendar.presentation.component
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.jorgediazp.kpopcalendar.main.calendar.presentation.model.DatePresentationModel
 import com.jorgediazp.kpopcalendar.main.calendar.presentation.model.SongPresentationType
@@ -62,6 +66,9 @@ fun SongsLazyColumn(listState: LazyListState, selectedDateIndex: Int, dateList: 
                     SongsEmptyCard()
                 }
             }
+        }
+        item {
+            Spacer(modifier = Modifier.fillMaxWidth().height(64.dp))
         }
     }
 
