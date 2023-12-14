bcb5490781e0d9ead559f7938aafd333328a4174
Jorge Díaz Pérez <jorge.diaz@alten.es>
2023-12-14 12:05:51 +0100

    handle states

--- app/src/main/java/com/jorgediazp/kpopcalendar/home/search/presentation/SearchViewModel.kt
+++ app/src/main/java/com/jorgediazp/kpopcalendar/home/search/presentation/SearchViewModel.kt
@@ -1,8 +1,8 @@
 package com.jorgediazp.kpopcalendar.home.search.presentation
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.jorgediazp.kpopcalendar.common.util.DataResult
 import com.jorgediazp.kpopcalendar.home.common.domain.GetSongsUseCase
 import com.jorgediazp.kpopcalendar.home.search.presentation.model.SearchScreenState
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -17,8 +17,22 @@ class SearchViewModel @Inject constructor(private val getSongsUseCase: GetSongsU
     val state = MutableStateFlow<SearchScreenState>(SearchScreenState.ShowNothing)
 
     fun loadSongListByQuery(query: String) {
-        viewModelScope.launch {
-            val result = getSongsUseCase.getSongListByQuery(query)
+        if (query.length > 1) {
+            viewModelScope.launch {
+                val result = getSongsUseCase.getSongListByQuery(query)
+                if (result is DataResult.Success && result.data != null) {
+                    if (result.data.isNotEmpty()) {
+                        state.value = SearchScreenState.ShowNothing
+
+                    } else {
+                        state.value = SearchScreenState.ShowNoResults
+                    }
+                } else {
+                    state.value = SearchScreenState.ShowError
+                }
+            }
+        } else {
+            state.value = SearchScreenState.ShowNothing
         }
     }
 }
