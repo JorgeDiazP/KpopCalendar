ec42394652e68d3a208e6b093d1103a0ff773fb4
Jorge Díaz Pérez <jorge.diaz@alten.es>
2024-01-07 20:11:30 +0100

    finish AboutScreen

--- app/src/main/java/com/jorgediazp/kpopcalendar/home/about/AboutScreen.kt
+++ app/src/main/java/com/jorgediazp/kpopcalendar/home/about/AboutScreen.kt
@@ -1,5 +1,8 @@
 package com.jorgediazp.kpopcalendar.home.about
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
@@ -13,9 +16,12 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.google.firebase.crashlytics.ktx.crashlytics
+import com.google.firebase.ktx.Firebase
 import com.jorgediazp.kpopcalendar.R
 import com.jorgediazp.kpopcalendar.common.presentation.theme.KpopCalendarTheme
 import com.jorgediazp.kpopcalendar.common.presentation.ui.Screen
@@ -23,6 +29,7 @@ import com.jorgediazp.kpopcalendar.common.presentation.ui.Screen
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AboutScreen() {
+    val context = LocalContext.current
     Screen {
         Column {
             TopAppBar(colors = TopAppBarDefaults.topAppBarColors(
@@ -45,12 +52,25 @@ fun AboutScreen() {
                     style = MaterialTheme.typography.bodyMedium,
                     modifier = Modifier.padding(32.dp)
                 )
-                Button(onClick = {}) {
+                Button(onClick = { launchPlayStore(context) }) {
                     Text(text = stringResource(id = R.string.about_button))
                 }
             }
         }
     }
+}
+
+private fun launchPlayStore(context: Context) {
+    try {
+        val intent = Intent(
+            Intent.ACTION_VIEW,
+            Uri.parse("market://details?id=${context.packageName}")
+        )
+        context.startActivity(intent)
+    } catch (e: Exception) {
+        Firebase.crashlytics.recordException(e)
+    }
+
 }
 
 @Preview
