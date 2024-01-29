d215676f35f2a23c6776894c3f4f3bb159802806
Jorge Díaz Pérez <jorge.diaz@alten.es>
2024-01-29 20:46:43 +0100

    add notifications

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -128,6 +128,7 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore")
     implementation("com.google.firebase:firebase-config")
     implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-messaging")
 
     // Navigation
     implementation("androidx.navigation:navigation-compose:2.7.5")
